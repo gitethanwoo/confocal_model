2012f32b5e2bf10ad0defe6591ec622f9ffa91d8
gitethanwoo <ewoo@albany.edu>
2020-11-04 13:09:51 -0500

    just clean up

--- shortModel.m
+++ shortModel.m
@@ -14,21 +14,25 @@ fitRange = (414:0.2:725.2)'; %this is our wavelength range for everything to fit
 [fitLasers, fitfilterSpectra, fluors] = fitassets(fitRange, lasers, fluors, filters);
 
 %%%% Change laser intensity to see how FOM changes
-laserIntensity = [0.247 0.3675 1]; % this is the controllable laser intensity (0-100%)
+laserIntensity = [1 0.3675 1]; % this is the controllable laser intensity (0-100%)
 %%%%
 
 [optimalInt, scaledBright] = excitationcalc(fitLasers, laserIntensity, fluors);
 
+%the following function will use the fluorophore spectra and laser
+%information to capture images of our fluorophore standards.
 [fluors, fluorStandard, channelVal, fluorsCombined] = captureimage(fluors, fitfilterSpectra, fitRange, scaledBright);
 
+%this just plots what our emission spectra will look like before filtering
 % plot(fitRange,fluorsCombined)
 
-test = lsqnonneg(fluorStandard', channelVal);
-
+%This function uses our fluor standards (pure fluors), our combined fluors
+%(which is what the microscope sees on a slide), and the filter spectra.
+%it then "captures" an image and unmixes it against our standards
 FOMS = captureUnmix(fluorStandard, fluorsCombined, fitfilterSpectra, fitRange);
 
 
+%the following lines just plot the FOM in a bar graph
 X = categorical({'488 FOM','568 FOM','647 FOM'});
-
 bar(X,FOMS)
 
