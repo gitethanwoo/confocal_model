7691f7d0271f39d557242c1a5f3bfb3773e0c7b0
gitethanwoo <ewoo@albany.edu>
2020-11-09 17:45:07 -0500

    Created a function to import all the filters. Edited other functions to work with structure of filters. Modified main code to select the filter.

--- shortModel.m
+++ shortModel.m
@@ -1,6 +1,6 @@
 %short model
 
-filters = readmatrix('assets/MBSZeiss.csv'); %import filter spectra into matrix
+filters = importfilters(); %import filter spectra into matrix
 
 wavelengths = [488 561 633]; %What lasers do we want to use?
 
@@ -11,17 +11,21 @@ fluors = importfluors(); %imports the fluorophores
 fitRange = (414:0.2:725.2)'; %this is our wavelength range for everything to fit to
 
 %the following function fits all the assets to our domain
-[fitLasers, fitfilterSpectra, fluors] = fitassets(fitRange, lasers, fluors, filters);
+[fitLasers, filters, fluors] = fitassets(fitRange, lasers, fluors, filters);
 
 %%%% Change laser intensity to see how FOM changes
 laserIntensity = [1 0.3675 1]; % this is the controllable laser intensity (0-100%)
 %%%%
 
+%Which filter?
+
+whichfilter = ['MBS'];
+
 [optimalInt, scaledBright] = excitationcalc(fitLasers, laserIntensity, fluors);
 
 %the following function will use the fluorophore spectra and laser
 %information to capture images of our fluorophore standards.
-[fluors, fluorStandard, channelVal, fluorsCombined] = captureimage(fluors, fitfilterSpectra, fitRange, scaledBright);
+[fluors, fluorStandard, channelVal, fluorsCombined] = captureimage(fluors, filters(1).fitSpectra, fitRange, scaledBright);
 
 %this just plots what our emission spectra will look like before filtering
 % plot(fitRange,fluorsCombined)
@@ -29,7 +33,7 @@ laserIntensity = [1 0.3675 1]; % this is the controllable laser intensity (0-100
 %This function uses our fluor standards (pure fluors), our combined fluors
 %(which is what the microscope sees on a slide), and the filter spectra.
 %it then "captures" an image and unmixes it against our standards
-FOMS = captureUnmix(fluorStandard, fluorsCombined, fitfilterSpectra, fitRange);
+FOMS = captureUnmix(fluorStandard, fluorsCombined, filters(1).fitSpectra, fitRange);
 
 
 %the following lines just plot the FOM in a bar graph
